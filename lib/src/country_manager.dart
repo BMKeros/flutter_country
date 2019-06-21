6629fedfbf65f31ba3707d5ddc42091f5a6ec1b0
Israel Lugo <hostelixisrael@gmail.com>
2019-06-21 12:51:04 -0400

    [ADD] add class country manager

---
+++ lib/src/country_manager.dart
@@ -0,0 +1,40 @@
+import '../resources/countries_data.dart';
+import 'models/country.dart';
+
+class CountryManager {
+  List<Country> getContries() {
+    return countriesData
+        .map((Map<String, String> data) => Country.fromMap(data))
+        .toList();
+  }
+
+  List<Map<String, String>> getNames() {
+    return countriesData.map((Map<String, String> data) {
+      return {'code': data['code'], 'name': data['name']};
+    }).toList();
+  }
+
+  List<String> getCodes() {
+    return countriesData
+        .map((Map<String, String> data) => data['code'])
+        .toList();
+  }
+
+  List<Map<String, String>> getCurrencies() {
+    return countriesData.map((Map<String, String> data) {
+      return {'code': data['code'], 'currency': data['currency']};
+    }).toList();
+  }
+
+  List<Map<String, String>> getDialCodes() {
+    return countriesData.map((Map<String, String> data) {
+      return {'code': data['code'], 'dial_code': data['dial_code']};
+    }).toList();
+  }
+
+  List<Map<String, String>> getCapitals() {
+    return countriesData.map((Map<String, String> data) {
+      return {'code': data['code'], 'capital': data['capital']};
+    }).toList();
+  }
+}
