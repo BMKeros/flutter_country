02a911b36920cbe8bd0b3fa36295ea2ad1da8525
boanergepro <antonycarrizo96@gmail.com>
2019-06-21 10:42:33 -0400

    [MOD]: Agregado json.

--- lib/flutter_country.dart
+++ lib/flutter_country.dart
@@ -1,7 +1,3 @@
 library flutter_country;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_country/resources/countries_data.dart';
